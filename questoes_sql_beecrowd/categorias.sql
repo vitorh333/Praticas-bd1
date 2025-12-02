f5c646cce982b14b38393de58d1acc5390c746e5
vh333 <vitor.hugo.dias.botelho.de.oliveira@ccc.ufcg.edu.br>
2025-12-02 20:56:35 -0300

    Realziando alguns exercicios

---
+++ questoes_sql_beecrowd/categorias.sql
@@ -0,0 +1,6 @@
+SELECT products.id, products.name
+FROM products
+JOIN categories
+	ON products.id_categories = categories.id
+
+WHERE categories.name LIKE 'super%';
---
+++ questoes_sql_beecrowd/menores_que_10_ou_maiores_que_100.sql
@@ -0,0 +1,3 @@
+SELECT id, name
+FROM products
+WHERE prime < 10 or price > 100;
